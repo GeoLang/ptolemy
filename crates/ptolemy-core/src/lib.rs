442e4231d7981dcb8fc5fe74f25462ec23f2a38c
TileTopia <tiletopia@proton.me>
2026-05-13 15:34:37 -0400

    Switch to AGPL-3.0, add GitHub Pages site
    - Replace MPL-2.0 with AGPL-3.0-or-later
    - Update all source file headers
    - Add docs/index.html (GitHub Pages, same style as TileTopia)

--- crates/ptolemy-core/src/lib.rs
+++ crates/ptolemy-core/src/lib.rs
@@ -1,6 +1,6 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
+// This Source Code Form is subject to the terms of the GNU Affero General Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+// file, You can obtain one at https://gnu.org/licenses/agpl-3.0.html.
 
 pub mod changeset;
 pub mod dataset;
